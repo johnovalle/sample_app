0e94b209fccde87fb68fcad5d0988cd5bdbc0a66
kugyousha <johnovalle@gmail.com>
2013-10-02 17:45:11 -0400

    Refactor tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
-	def full_title(page_tile)
+	def full_title(page_title)
 		base_title = "Ruby on Rails Tutorial Sample App"
-		if base_title.empty?
+		if page_title.empty?
 			base_title
 		else
-			"#{base_title} | #{page_tile}"
+			"#{base_title} | #{page_title}"
 		end
 	end
 end
