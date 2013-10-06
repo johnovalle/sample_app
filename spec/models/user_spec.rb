b51d967bab0747c10cd74e91a595862c1b439bec
kugyousha <johnovalle@gmail.com>
2013-10-06 18:56:43 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
 
@@ -106,6 +107,9 @@ describe User do
   	it { should be_invalid }
   end
 
-
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
   
 end
